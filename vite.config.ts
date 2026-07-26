519fd0f28dcf8caeb0a14d027199ce26aa5248a8
omjee <coffeeatsea80@gmail.com>
2026-07-27 03:34:10 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PORTFOLIO/',
+  base: '/PORTFOLIO.github.io/',
   build: {
     outDir: 'docs',
   },
